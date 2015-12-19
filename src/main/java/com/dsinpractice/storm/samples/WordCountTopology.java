c963685325c57d3daf1276703efa258c1cc98d96
Hemanth Yamijala <hyamijala@hortonworks.com>
2015-12-19 13:58:03 +0530

    Hive bolt

--- src/main/java/com/dsinpractice/storm/samples/WordCountTopology.java
+++ src/main/java/com/dsinpractice/storm/samples/WordCountTopology.java
@@ -3,18 +3,25 @@ package com.dsinpractice.storm.samples;
 import backtype.storm.Config;
 import backtype.storm.LocalCluster;
 import backtype.storm.StormSubmitter;
+import backtype.storm.spout.SchemeAsMultiScheme;
 import backtype.storm.task.ShellBolt;
-import backtype.storm.topology.BasicOutputCollector;
-import backtype.storm.topology.IRichBolt;
-import backtype.storm.topology.OutputFieldsDeclarer;
-import backtype.storm.topology.TopologyBuilder;
+import backtype.storm.topology.*;
 import backtype.storm.topology.base.BaseBasicBolt;
 import backtype.storm.tuple.Fields;
 import backtype.storm.tuple.Tuple;
 import backtype.storm.tuple.Values;
+import org.apache.storm.hive.bolt.HiveBolt;
+import org.apache.storm.hive.bolt.mapper.DelimitedRecordHiveMapper;
+import org.apache.storm.hive.bolt.mapper.HiveMapper;
+import org.apache.storm.hive.common.HiveOptions;
+import storm.kafka.KafkaSpout;
+import storm.kafka.SpoutConfig;
+import storm.kafka.StringScheme;
+import storm.kafka.ZkHosts;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 
 /**
  * This topology demonstrates Storm's stream groupings and multilang capabilities.
@@ -48,12 +55,12 @@ public class WordCountTopology {
                 count = 0;
             count++;
             counts.put(word, count);
-            collector.emit(new Values(word, count));
+            collector.emit(new Values(word, count, word.charAt(0)));
         }
 
         @Override
         public void declareOutputFields(OutputFieldsDeclarer declarer) {
-            declarer.declare(new Fields("word", "count"));
+            declarer.declare(new Fields("word", "count", "letter"));
         }
     }
 
@@ -61,10 +68,20 @@ public class WordCountTopology {
 
         TopologyBuilder builder = new TopologyBuilder();
 
-        builder.setSpout("spout", new RandomSentenceSpout(), 5);
+        builder.setSpout("kafka-spout", getKafkaSpout(), 1);
 
-        builder.setBolt("split", new SplitSentence(), 8).shuffleGrouping("spout");
-        builder.setBolt("count", new WordCount(), 12).fieldsGrouping("split", new Fields("word"));
+        builder.setBolt("split", new SplitSentence(), 2).shuffleGrouping("kafka-spout");
+
+        builder.setBolt("count", new WordCount(), 2).fieldsGrouping("split", new Fields("word"));
+        HiveMapper mapper = new DelimitedRecordHiveMapper()
+                .withColumnFields(new Fields("word", "count"))
+                .withPartitionFields(new Fields("letter"));
+
+        HiveOptions options = new HiveOptions("thrift://localhost:9083", "default", "storm_words_table", mapper)
+                .withBatchSize(100)
+                .withTxnsPerBatch(10)
+                .withIdleTimeout(10);
+        builder.setBolt("hive-bolt", new HiveBolt(options), 2).shuffleGrouping("count");
 
         Config conf = new Config();
         conf.setDebug(true);
@@ -85,5 +102,13 @@ public class WordCountTopology {
 
             cluster.shutdown();
         }
+    }
+
+    private static IRichSpout getKafkaSpout() {
+        ZkHosts zkHosts = new ZkHosts("localhost:2181");
+        String topicName = "test-topic";
+        SpoutConfig spoutConfig = new SpoutConfig(zkHosts, topicName, "/" + topicName, UUID.randomUUID().toString());
+        spoutConfig.scheme = new SchemeAsMultiScheme(new StringScheme());
+        return new KafkaSpout(spoutConfig);
     }
 }
